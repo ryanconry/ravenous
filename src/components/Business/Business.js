dd572f02b9f490f5d553113f55c98fc2f8f3eef0
ryanconry <r.conry@hotmail.com>
2017-10-21 12:51:46 -0400

    using props to pass businesses' info to child components

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import './Business.css';
 
-const business = {           //create an object to hold data for a business
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
 class Business extends React.Component {
   render(){
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.imageSrc} alt=''/>
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>{Business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{Business.address}</p>
-            <p>{Business.city}</p>
-            <p>{Business.state} {Business.zipCode}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {Business.zipCode}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{Business.category}</h3>
-            <h3 className="rating">{Business.rating} stars</h3>
-            <p>{Business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating} stars</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
