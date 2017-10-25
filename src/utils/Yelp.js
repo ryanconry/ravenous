c73e5090152b3be7e101f695896ea681a067265d
ryanconry <r.conry@hotmail.com>
2017-10-24 20:50:33 -0400

    pushing with updated search function

--- src/utils/Yelp.js
+++ src/utils/Yelp.js
@@ -11,28 +11,29 @@ const Yelp = {
      + clientId + '&client_secret=' + secret,{method: 'POST'}).then(response =>
         {return response.json()}).then(jsonResponse => {accessToken = jsonResponse.access_token;});
   },
-  search(term,location,sortBy){
-    return Yelp.getAccessToken().then(() => {return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term='+
-     term + '&location=' + location + '&sort_by=' + sortBy, {headers: {Authorization: `Bearer ${accessToken}`}})}).then(
-       jsonResponse => {
-         if(jsonResponse.business){
-           return jsonResponse.business.map(business => {
-             return{
-               id: business.id,
-               imageSrc: business.imageSrc,
-               name: business.name,
-               address: business.address,
-               city: business.city,
-               state: business.state,
-               zipCode: business.zipCode,
-               category: business.category,
-               rating: business.rating,
-               reviewCount: business.reviewCount
-             }
-           })
-         }
-       }
-     )
-  }
-}
-export default Yelp;
+  search(term, location, sortBy) {
+      return Yelp.getAccessToken().then(() => {
+        return fetch(https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}, {
+          headers: {
+            Authorization: Bearer ${accessToken}
+          }
+        });
+      }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }));
+        }
+      });
+    }
