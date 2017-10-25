f6b732bd6c4a6df9bd0dbaab0b70a15d405434c9
ryanconry <r.conry@hotmail.com>
2017-10-24 20:11:02 -0400

    updated setState in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   searchYelp(term,location,sortBy){
     Yelp.search(term,location,sortBy).then(businesses => {
-      this.setState(businesses: businesses)
+      this.setState({businesses: businesses})
     });
   }
 
